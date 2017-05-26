23ffcc7d17a31050c29870c3b20152120ec1c1b8
ikatyang <ikatyang@gmail.com>
2017-05-26 17:22:24 +0800

    Update tests

--- tests/replace.ts
+++ tests/replace.ts
@@ -1,13 +1,18 @@
 import * as R from 'ramda';
 
 // @dts-jest
-R.replace(R.__, 'bar');
-
+R.replace('foo', 'bar', 'foo foo foo'); // => 'bar foo foo'
 // @dts-jest
-R.replace('foo', 'bar');
-
+R.replace('foo', 'bar')('foo foo foo'); // => 'bar foo foo'
 // @dts-jest
-R.replace('foo')('bar');
-
+R.replace('foo')('bar')('foo foo foo'); // => 'bar foo foo'
 // @dts-jest
-R.replace(/foo/, 'bar', 'foo foo foo');
+R.replace(/foo/, 'bar', 'foo foo foo'); // => 'bar foo foo'
+
+// Use the 'g' (global) flag to replace all occurrences:
+// @dts-jest
+R.replace(/foo/g, 'bar', 'foo foo foo'); // => 'bar bar bar'
+// @dts-jest
+R.replace(/foo/g, 'bar')('foo foo foo'); // => 'bar bar bar'
+// @dts-jest
+R.replace(/foo/g)('bar')('foo foo foo'); // => 'bar bar bar'
