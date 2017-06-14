1aa7dee35da63a06ed1746ca5a0c903f21c7de28
ikatyang <ikatyang@gmail.com>
2017-06-14 18:40:12 +0800

    Fix typo

--- tests/all.ts
+++ tests/all.ts
@@ -1,10 +1,10 @@
 import {Predicate} from 'ramda/src/$types';
 import * as R_all from 'ramda/src/all';
 
-declare const string_predicater: Predicate<string>;
+declare const string_predicate: Predicate<string>;
 declare const string_array: string[];
 
 // @dts-jest
-R_all(string_predicater)(string_array);
+R_all(string_predicate)(string_array);
 // @dts-jest
-R_all(string_predicater, string_array);
+R_all(string_predicate, string_array);
--- tests/any.ts
+++ tests/any.ts
@@ -1,10 +1,10 @@
 import {Predicate} from 'ramda/src/$types';
 import * as R_any from 'ramda/src/any';
 
-declare const string_predicater: Predicate<string>;
+declare const string_predicate: Predicate<string>;
 declare const string_array: string[];
 
 // @dts-jest
-R_any(string_predicater)(string_array);
+R_any(string_predicate)(string_array);
 // @dts-jest
-R_any(string_predicater, string_array);
+R_any(string_predicate, string_array);
