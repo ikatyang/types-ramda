e5ebe524e95eb29e67179deb670ce6edcf6cd8c9
ikatyang <ikatyang@gmail.com>
2017-06-14 23:45:50 +0800

    Fix signature-sort-order remain original order

--- templates/utils/sort-signatures.ts
+++ templates/utils/sort-signatures.ts
@@ -2,7 +2,7 @@ import * as dts from 'dts-element';
 import {placeholder_name_abbr} from './constants';
 
 export const sort_signatures = (signatures: dts.IObjectMember[]) => {
-  const normals: dts.IObjectMember[] = [];
+  const normals: {value: dts.IObjectMember, index: number}[] = [];
   const selectables: dts.IObjectMember[] = [];
   signatures.forEach(signature => {
     const function_decalration = signature.owned as dts.IFunctionDeclaration;
@@ -11,22 +11,22 @@ export const sort_signatures = (signatures: dts.IObjectMember[]) => {
     if ((function_type.parameters || []).length === 0) {
       selectables.push(signature);
     } else {
-      normals.push(signature);
+      normals.push({value: signature, index: normals.length});
     }
   });
-  normals.sort((member1, member2) => {
-    const function_type_1 = (member1.owned as dts.IFunctionDeclaration).type!;
-    const function_type_2 = (member2.owned as dts.IFunctionDeclaration).type!;
+  const members = normals.slice().sort((normal1, normal2) => {
+    const function_type_1 = (normal1.value.owned as dts.IFunctionDeclaration).type!;
+    const function_type_2 = (normal2.value.owned as dts.IFunctionDeclaration).type!;
     const parameters_1 = function_type_1.parameters!;
     const parameters_2 = function_type_2.parameters!;
     return (parameters_1.length !== parameters_2.length)
       ? get_length_order(parameters_2.length) - get_length_order(parameters_1.length)
-      : parameters_2.findIndex(is_placeholder) - parameters_1.findIndex(is_placeholder);
-  });
+      : (parameters_2.findIndex(is_placeholder) - parameters_1.findIndex(is_placeholder)) || (normal1.index - normal2.index);
+  }).map(normal => normal.value);
   return [
-    ...normals.slice(0, -1),
+    ...members.slice(0, -1),
     ...selectables,
-    ...normals.slice(-1),
+    ...members.slice(-1),
   ];
 
   function is_placeholder(parameter: dts.IParameterDeclaration) {
