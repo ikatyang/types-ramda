c3c167b90d28b53af115f552ee1b745494b8bc33
ikatyang <ikatyang@gmail.com>
2017-06-18 21:41:38 +0800

    Update utils add create-composition-declarations

---
+++ templates/utils/create-composition-declarations.ts
@@ -0,0 +1,54 @@
+import * as dts from 'dts-element';
+
+export const create_composition_declarations = (
+    name: string,
+    kind: 'compose' | 'pipe',
+    max_function_count: number,
+    max_parameter_count: number,
+    generate_parameter_type: (generic: string) => string = x => x,
+    generate_return_type: (generic: string) => string = x => x,
+    ) => {
+  const function_names = [...new Array(max_function_count)].map((_, index) => `fn${index + 1}`);
+  const return_generics = [...new Array(max_function_count)].map((_, index) => `R${index + 1}`);
+
+  const parameter_names = [...new Array(max_parameter_count)].map((_, index) => `v${index + 1}`);
+  const parameter_generics = [...new Array(max_parameter_count)].map((_, index) => `V${index + 1}`);
+
+  const declarations = [`declare const ${name}: typeof $;`];
+  for (let function_count = 1; function_count <= max_function_count; function_count++) {
+    const entry_index = (kind === 'pipe') ? 0 : function_count - 1;
+    const current_return_generics = return_generics.slice(0, function_count);
+
+    const current_sorted_return_generics = current_return_generics.slice();
+    const current_sorted_function_names = function_names.slice(0, function_count);
+    if (kind === 'compose') {
+      current_sorted_return_generics.reverse();
+      current_sorted_function_names.reverse();
+    }
+
+    for (let parameter_count = 1; parameter_count <= max_parameter_count; parameter_count++) {
+      const current_parameter_names = parameter_names.slice(0, parameter_count);
+      const current_parameter_generics = parameter_generics.slice(0, parameter_count);
+
+      const entry_parameters = current_parameter_names.map((parameter_name, param_index) => `
+        ${parameter_name}: ${generate_parameter_type(current_parameter_generics[param_index])}
+      `).join(',');
+
+      declarations.push(`
+        declare function $<${[...current_parameter_generics, ...current_return_generics].join(',')}>(
+          ${current_return_generics.map((_, index) => {
+            const function_name = current_sorted_function_names[index];
+            const return_generic = current_sorted_return_generics[index];
+            return (index === entry_index)
+              ? `${function_name}: (${entry_parameters}) => ${generate_return_type(return_generic)}`
+              : `${function_name}: (v: ${
+                generate_parameter_type(current_sorted_return_generics[index + (kind === 'compose' ? 1 : -1)])
+              }) => ${generate_return_type(return_generic)}`;
+          }).join(',')}
+        ): (${entry_parameters}) => ${generate_return_type(current_return_generics[current_return_generics.length - 1])};
+      `);
+    }
+  }
+
+  return dts.parse(declarations.join('\n')).members;
+};
