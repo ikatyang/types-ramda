eddde73824716baeb39a0c8b29ec5999dc44ffe0
ikatyang <ikatyang@gmail.com>
2017-06-20 23:28:15 +0800

    Update R.addIndex support 0~4 additional paramter for iterFn

--- templates/addIndex.d.ts
+++
@@ -1,5 +0,0 @@
-import {CurriedFunction2} from './$curried-functions';
-import {IndexedListMorphism, IndexedObjectMorphism, List, ListMapper, ObjectMapper} from './$types';
-
-export function $list<T, U>(fn: ListMapper<T, U>): CurriedFunction2<IndexedListMorphism<T, U>, List<T>, U[]>;
-export function $object<T, U, K extends string>(fn: ObjectMapper<T, U, K>): CurriedFunction2<IndexedObjectMorphism<T, U, K>, Record<K, T>, Record<K, U>>;
