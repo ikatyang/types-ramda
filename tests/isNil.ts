49ac900e04683fa53e85927152ebfee048921ec4
ikatyang <ikatyang@gmail.com>
2017-06-22 00:14:29 +0800

    Add tslint.json for tests

--- tests/isNil.ts
+++ tests/isNil.ts
@@ -3,7 +3,7 @@ import * as R_isNil from 'ramda/src/isNil';
 declare const string: string;
 
 // @dts-jest:pass
-R_isNil; // tslint:disable-line:no-unused-expression
+R_isNil;
 
 // @dts-jest:pass
 R_isNil(string);
