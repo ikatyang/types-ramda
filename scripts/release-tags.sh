f04604a84936de9c848f93f3f9841d48015035d6
ikatyang <ikatyang@gmail.com>
2017-06-28 11:03:50 +0800

    chore(scripts): add release-tags script

---
+++ scripts/release-tags.sh
@@ -0,0 +1,8 @@
+git fetch --all
+
+git tag "v$npm_package_version-dist" origin/dist
+git tag "v$npm_package_version-dist-simple" origin/dist-simple
+git tag "v$npm_package_version-dist-selectable" origin/dist-selectable
+git tag "v$npm_package_version-dist-placeholder" origin/dist-placeholder
+
+git push --tags
