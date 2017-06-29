8e35e2175e54ed76f590605ab48689726d2c6e9d
ikatyang <ikatyang@gmail.com>
2017-06-30 00:48:28 +0800

    test(utils): add tests for n-ary-declarations

--- templates/utils/__tests__/create-n-ary-declarations.ts
+++ templates/utils/__tests__/create-n-ary-declarations.ts
@@ -1,7 +1,20 @@
 import * as dts from 'dts-element';
 import {create_n_ary_declarations} from '../create-n-ary-declarations';
 
-it('should generate correctly', () => {
+it('should generate correctly without generate_additional', () => {
+  expect(emit_n_ary_declarations()).toMatchSnapshot();
+});
+
+it('should generate correctly with generate_additional', () => {
+  expect(emit_n_ary_declarations(args => `
+    function $variadic<${args.return_type}>(
+      n: number,
+      fn: (...args: any[]) => ${args.return_type}
+    ): (...args: any[]) => ${args.return_type};
+  `)).toMatchSnapshot();
+});
+
+function emit_n_ary_declarations(generate_additional?: (args: any) => string) {
   const n_ary_declarations = create_n_ary_declarations(
     0,
     3,
@@ -11,13 +24,8 @@ it('should generate correctly', () => {
         fn: (${[...args.parameters, '...args: any[]'].join(',')}) => ${args.return_type}
       ): (${args.parameters.join(',')}) => ${args.return_type};
     `,
-    args => `
-      function $variadic<${args.return_type}>(
-        n: number,
-        fn: (...args: any[]) => ${args.return_type}
-      ): (...args: any[]) => ${args.return_type};
-    `,
+    generate_additional,
   );
   const top_level_element = dts.create_top_level_element({members: n_ary_declarations});
-  expect(dts.emit(top_level_element)).toMatchSnapshot();
-});
+  return dts.emit(top_level_element);
+}
