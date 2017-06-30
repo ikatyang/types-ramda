e106e1c2d68f3374de918156404a69e4bf4b630e
Ika <noreply@github.com>
2017-06-30 15:36:54 +0000

    deploy(dist): d62276675e4ead376d217a358a6a950ca1dbf737

--- src/ascend.d.ts
+++ src/ascend.d.ts
@@ -24,53 +24,53 @@ import { Placeholder as PH } from "./$placeholder";
  */
 declare const ascend: ascend_000;
 type ascend_000 = {
-    <T, U extends Ordered>(fn: Morphism<T, U>, _a: PH, b: T): ascend_101<T, U>;
+    <T>(fn: Morphism<T, Ordered>, _a: PH, b: T): ascend_101<T>;
     <T>(_fn: PH, a: T, b: T): ascend_011<T>;
     <T>(_fn: PH, _a: PH, b: T): ascend_001<T>;
-    <T, U extends Ordered>(fn: Morphism<T, U>, a: T, b: T): ascend_111;
+    <T>(fn: Morphism<T, Ordered>, a: T, b: T): ascend_111;
     <T>(_fn: PH, a: T): ascend_010<T>;
-    <T, U extends Ordered>(fn: Morphism<T, U>, a: T): ascend_110<T, U>;
-    <$SEL extends "111">(): <T, U extends Ordered>(fn: Morphism<T, U>, a: T, b: T) => ascend_111;
-    <$SEL extends "11">(): <T, U extends Ordered>(fn: Morphism<T, U>, a: T) => ascend_110<T, U>;
-    <$SEL extends "101">(): <T, U extends Ordered>(fn: Morphism<T, U>, _a: PH, b: T) => ascend_101<T, U>;
-    <$SEL extends "1">(): <T, U extends Ordered>(fn: Morphism<T, U>) => ascend_100<T, U>;
+    <T>(fn: Morphism<T, Ordered>, a: T): ascend_110<T>;
+    <$SEL extends "111">(): <T>(fn: Morphism<T, Ordered>, a: T, b: T) => ascend_111;
+    <$SEL extends "11">(): <T>(fn: Morphism<T, Ordered>, a: T) => ascend_110<T>;
+    <$SEL extends "101">(): <T>(fn: Morphism<T, Ordered>, _a: PH, b: T) => ascend_101<T>;
+    <$SEL extends "1">(): <T>(fn: Morphism<T, Ordered>) => ascend_100<T>;
     <$SEL extends "011">(): <T>(_fn: PH, a: T, b: T) => ascend_011<T>;
     <$SEL extends "01">(): <T>(_fn: PH, a: T) => ascend_010<T>;
     <$SEL extends "001">(): <T>(_fn: PH, _a: PH, b: T) => ascend_001<T>;
-    <T, U extends Ordered>(fn: Morphism<T, U>): ascend_100<T, U>;
+    <T>(fn: Morphism<T, Ordered>): ascend_100<T>;
 };
-type ascend_100<T, U extends Ordered> = {
-    (_a: PH, b: T): ascend_101<T, U>;
+type ascend_100<T> = {
+    (_a: PH, b: T): ascend_101<T>;
     (a: T, b: T): ascend_111;
     <$SEL extends "11">(): (a: T, b: T) => ascend_111;
-    <$SEL extends "1">(): (a: T) => ascend_110<T, U>;
-    <$SEL extends "01">(): (_a: PH, b: T) => ascend_101<T, U>;
-    (a: T): ascend_110<T, U>;
+    <$SEL extends "1">(): (a: T) => ascend_110<T>;
+    <$SEL extends "01">(): (_a: PH, b: T) => ascend_101<T>;
+    (a: T): ascend_110<T>;
 };
 type ascend_010<T> = {
     (_fn: PH, b: T): ascend_011<T>;
-    <U extends Ordered>(fn: Morphism<T, U>, b: T): ascend_111;
-    <$SEL extends "11">(): <U extends Ordered>(fn: Morphism<T, U>, b: T) => ascend_111;
-    <$SEL extends "1">(): <U extends Ordered>(fn: Morphism<T, U>) => ascend_110<T, U>;
+    (fn: Morphism<T, Ordered>, b: T): ascend_111;
+    <$SEL extends "11">(): (fn: Morphism<T, Ordered>, b: T) => ascend_111;
+    <$SEL extends "1">(): (fn: Morphism<T, Ordered>) => ascend_110<T>;
     <$SEL extends "01">(): (_fn: PH, b: T) => ascend_011<T>;
-    <U extends Ordered>(fn: Morphism<T, U>): ascend_110<T, U>;
+    (fn: Morphism<T, Ordered>): ascend_110<T>;
 };
-type ascend_110<T, U extends Ordered> = {
+type ascend_110<T> = {
     (b: T): ascend_111;
 };
 type ascend_001<T> = {
     (_fn: PH, a: T): ascend_011<T>;
-    <U extends Ordered>(fn: Morphism<T, U>, a: T): ascend_111;
-    <$SEL extends "11">(): <U extends Ordered>(fn: Morphism<T, U>, a: T) => ascend_111;
-    <$SEL extends "1">(): <U extends Ordered>(fn: Morphism<T, U>) => ascend_101<T, U>;
+    (fn: Morphism<T, Ordered>, a: T): ascend_111;
+    <$SEL extends "11">(): (fn: Morphism<T, Ordered>, a: T) => ascend_111;
+    <$SEL extends "1">(): (fn: Morphism<T, Ordered>) => ascend_101<T>;
     <$SEL extends "01">(): (_fn: PH, a: T) => ascend_011<T>;
-    <U extends Ordered>(fn: Morphism<T, U>): ascend_101<T, U>;
+    (fn: Morphism<T, Ordered>): ascend_101<T>;
 };
-type ascend_101<T, U extends Ordered> = {
+type ascend_101<T> = {
     (a: T): ascend_111;
 };
 type ascend_011<T> = {
-    <U extends Ordered>(fn: Morphism<T, U>): ascend_111;
+    (fn: Morphism<T, Ordered>): ascend_111;
 };
 type ascend_111 = number;
 export = ascend;
