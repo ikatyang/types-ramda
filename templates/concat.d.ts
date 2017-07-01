b9fc8cbc458b8c47510b95ef1a8d90e1be697e10
ikatyang <ikatyang@gmail.com>
2017-07-01 12:52:49 +0800

    fix(template): concat is not compatible with List

--- templates/concat.d.ts
+++ templates/concat.d.ts
@@ -1,3 +1,4 @@
 import {List} from './$types';
 
-export function $<T extends List<any>>(a: T, b: T): T;
+export function $string(a: string, b: string): string;
+export function $<T>(a: T[], b: T[]): T[];
