53cabcea314a563bfb0c79c73db554b8cc359528
ikatyang <ikatyang@gmail.com>
2017-07-01 05:13:52 +0000

    deploy(dist-simple): b9fc8cbc458b8c47510b95ef1a8d90e1be697e10

--- src/concat.d.ts
+++ src/concat.d.ts
@@ -27,14 +27,23 @@ import { List } from "./$types";
  */
 declare const concat: concat_00;
 type concat_00 = {
-    <T extends List<any>>(a: T, b: T): concat_11<T>;
-    <T extends List<any>>(a: T): concat_10<T>;
+    (a: string, b: string): concat_string_11;
+    <T>(a: T[], b: T[]): concat__11<T>;
+    (a: string): concat_string_10;
+    <T>(a: T[]): concat__10<T>;
 };
-type concat_10<T extends List<any>> = {
-    (b: T): concat_11<T>;
+type concat_string_10 = {
+    (b: string): concat_string_11;
 };
-type concat_01<T extends List<any>> = {
-    (a: T): concat_11<T>;
+type concat__10<T> = {
+    (b: T[]): concat__11<T>;
 };
-type concat_11<T extends List<any>> = T;
+type concat_string_01 = {
+    (a: string): concat_string_11;
+};
+type concat__01<T> = {
+    (a: T[]): concat__11<T>;
+};
+type concat_string_11 = string;
+type concat__11<T> = T[];
 export = concat;
