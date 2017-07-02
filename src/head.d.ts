7e1cb3af5264ee80dcd481b6ccd9f16838be19c6
ikatyang <ikatyang@gmail.com>
2017-07-02 07:02:49 +0000

    deploy(dist-simple): efc2ea752e50a29a20157606c237a91bce0590a5

--- src/head.d.ts
+++ src/head.d.ts
@@ -23,8 +23,10 @@ import { List } from "./$types";
 declare const head: head_0;
 type head_0 = {
     (str: string): head_string_1;
-    <T>(list: List<T>): head_list_1<T>;
+    <T extends [any]>(list: T): head_tuple_1<T>;
+    <T extends List<any>>(list: T): head_list_1<T>;
 };
 type head_string_1 = string;
-type head_list_1<T> = T | undefined;
+type head_tuple_1<T extends [any]> = T[0];
+type head_list_1<T extends List<any>> = T[0] | undefined;
 export = head;
