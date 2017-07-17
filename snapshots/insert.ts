f9681397958725dddd1c6d300342901599f7f0cb
ikatyang <ikatyang@gmail.com>
2017-07-17 14:59:16 +0800

    fix(insert): reorder generics

--- snapshots/insert.ts
+++ snapshots/insert.ts
@@ -5,14 +5,14 @@ declare const object_array: object[];
 declare const string: string;
 declare const number: number;
 
-// @dts-jest:pass -> <T>(list: T[] | ArrayLike<T>) => (string | T)[]
+// @dts-jest:pass -> <U>(list: U[] | ArrayLike<U>) => (string | U)[]
 R_insert(number, string);
 // @dts-jest:pass -> (string | object)[]
 R_insert(number, string, object_array);
 // @dts-jest:pass -> (string | object)[]
 R_insert(number)(string)(object_array);
 
-// @dts-jest:pass -> <T>(list: T[] | ArrayLike<T>) => (object | T)[]
+// @dts-jest:pass -> <U>(list: U[] | ArrayLike<U>) => (object | U)[]
 R_insert(number, object);
 // @dts-jest:pass -> object[]
 R_insert(number, object, object_array);
