560e6e35e3b0ecd6869c30fc93d5fa71aa6e5d96
ikatyang <ikatyang@gmail.com>
2017-08-01 21:15:03 +0800

    fix(temlpates): fix `Cannot find name 'K'` for `dissoc` and `pick`

--- templates/dissoc.d.ts
+++ templates/dissoc.d.ts
@@ -5,8 +5,8 @@ export function $keyof<T extends {}, K extends keyof T>(
   property: K,
   object: T,
 ): Omit<T, K>;
-export function $record<T extends Record<K, any>, K extends string>(
-  property: K,
-  object: T,
-): Omit<T, K>;
+export function $record<U extends Record<V, any>, V extends string>(
+  property: V,
+  object: U,
+): Omit<U, V>;
 export function $general<T extends {}>(property: Property, object: T): T;
--- templates/pick.d.ts
+++ templates/pick.d.ts
@@ -4,10 +4,10 @@ export function $keyof<T extends {}, K extends keyof T>(
   keys: List<K>,
   object: T,
 ): Pick<T, K>;
-export function $record<T extends Record<K, any>, K extends string>(
-  keys: List<K>,
-  object: T,
-): Pick<T, K>;
+export function $record<U extends Record<V, any>, V extends string>(
+  keys: List<V>,
+  object: U,
+): Pick<U, V>;
 export function $general<T extends {}>(
   keys: List<Property>,
   object: T,
