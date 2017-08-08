4f613d703dda85c3863888199bb14ad24f9730d4
Ika <ikatyang@gmail.com>
2017-08-08 14:52:28 +0800

    feat(unless): support predicate (#87)

--- tests/unless.ts
+++ tests/unless.ts
@@ -1,5 +1,6 @@
 import * as R_unless from '../ramda/dist/src/unless';
 
+declare const number_is_1: (x: number) => x is 1;
 declare const number_to_boolean: (x: number) => boolean;
 declare const number_to_string: (x: number) => string;
 declare const number: number;
@@ -10,3 +11,10 @@ R_unless(number_to_boolean, number_to_string);
 R_unless(number_to_boolean)(number_to_string)(number);
 // @dts-jest:pass
 R_unless(number_to_boolean, number_to_string, number);
+
+// @dts-jest:pass
+R_unless(number_is_1, number_to_string);
+// @dts-jest:pass
+R_unless(number_is_1)(number_to_string)(number);
+// @dts-jest:pass
+R_unless(number_is_1, number_to_string, number);
